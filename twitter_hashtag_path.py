27b90b238d9e0582505465ee602a8ec605355ce8
nceccarelli <njceccar@buffalo.edu>
2019-02-15 00:46:47 -0500

    few changes

--- twitter_hashtag_path.py
+++ twitter_hashtag_path.py
@@ -2,10 +2,10 @@ import tweepy
 import csv
 #import pandas as pd
 ####input your credentials here
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key = '8mh6Lmj8C3Scqtyb9ClI2bu28'
+consumer_secret = 'A5vGeamUE68bWUllD5Z4kx2ddiHaaRUcGWsijnunvP5W4Amb92'
+access_token = '1027798144589881344-2R13pJvgT7qsoOuMV9hgnpYKY9c6rb'
+access_token_secret = '2M8uP0Ju25aBqxnrGRdzMcJGyjOmxqbxPpYX4TnrS5L13'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -23,8 +23,10 @@ while True :
     
         if (tweet.text != prevTweet):
             #do things
-            print("r")
+            print("new tweet")
         prevTweet = tweet.text
+        print("old tweet")
+        #time.sleep(5)
     #print(tweet.created_at, tweet.text)
     #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
