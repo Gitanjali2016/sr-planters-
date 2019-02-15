7319d807474a5fadd82246decb6f194ded1bcdf1
nceccarelli <njceccar@buffalo.edu>
2019-02-15 16:38:30 -0500

    reading tweets

--- twitter_hashtag_path.py
+++ twitter_hashtag_path.py
@@ -1,32 +1,30 @@
 import tweepy
-import csv
-#import pandas as pd
-####input your credentials here
-consumer_key = '8mh6Lmj8C3Scqtyb9ClI2bu28'
-consumer_secret = 'A5vGeamUE68bWUllD5Z4kx2ddiHaaRUcGWsijnunvP5W4Amb92'
-access_token = '1027798144589881344-2R13pJvgT7qsoOuMV9hgnpYKY9c6rb'
-access_token_secret = '2M8uP0Ju25aBqxnrGRdzMcJGyjOmxqbxPpYX4TnrS5L13'
+import time
 
+#Twitter info
+consumer_key = '5evr04VvMIbVnPPcYH9Acavbz'
+consumer_secret = 'Szu0gUJhG8vuvD3lY4YGu37JPgd1XkZ4waEXCsniUU147f6g9S'
+access_token = '1027798144589881344-84yC8343RgvE6q7IiBmMkN0VItw01h'
+access_token_secret = 'Ou67UFrWBgq7N7WuqrH5quK9D5wyxANvAZ6S7ZzZSCdX4'
+
+#Manage Twitter data
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-#####United Airlines
-# Open/Create a file to append data
-#csvFile = open('ua.csv', 'a')
-#Use csv Writer
-#csvWriter = csv.writer(csvFile)
 
+#Pull from Twitter 
 prevTweet = ""
+search = "Yeah no if you need anything you need me a little while to do something else I think it would probably work better if I had to do it I just got a new phone call me "
 while True :
-    for tweet in tweepy.Cursor(api.search,q="#refugees",count=1,
-                           lang="en").items():
+    for tweet in tweepy.Cursor(api.search,q=search).items():
     
         if (tweet.text != prevTweet):
             #do things
-            print("new tweet")
+            print("*************************************")
+            print("new tweet -" +  "CONTENTS: " + tweet.text)
         prevTweet = tweet.text
-        print("old tweet")
-        #time.sleep(5)
+        print("old tweet -" +  "CONTENTS: " + tweet.text)
+        time.sleep(1)
     #print(tweet.created_at, tweet.text)
     #csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
